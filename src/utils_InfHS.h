e7659d874700c9c6939f855f45f70bc11cd9ed47
cbusatto <122295393+cbusatto@users.noreply.github.com>
2025-05-08 12:18:38 +0200

    Add files via upload

---
+++ src/utils_InfHS.h
@@ -0,0 +1,13 @@
+/* File: utils_InfHS.h */
+
+#ifndef utils_InfHS_H
+#define utils_InfHS_H
+
+
+double rlambda (const double& d, const double& a, const double& b, const double& lambda_last);
+
+Eigen::VectorXd repelem (const double& x, const int& n);
+Eigen::VectorXd rmvnorm (const Eigen::VectorXd& m, const Eigen::MatrixXd& XtX, const Eigen::VectorXd& d, const double& sigma2, const int& n); 
+Eigen::VectorXd rmvnorm_b (const Eigen::MatrixXd& X, const Eigen::VectorXd& y, const Eigen::VectorXd& d, const double& sigma2);
+
+#endif
